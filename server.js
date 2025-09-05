c13597fe3a7bc35314a1914542a2a4b27c2cc3aa
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-05 21:47:46 +0330

    Updated MongoDB connection log messages to English.

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const mongoDBURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/phan_ti
 
 // Connect to MongoDB
 mongoose.connect(mongoDBURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Kết nối tới MongoDB thành công!'))
-  .catch(err => console.error('Không thể kết nối tới MongoDB:', err));
+  .then(() => console.log('Successfully connected to MongoDB!'))
+  .catch(err => console.error('Unable to connect to MongoDB:', err));
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server đang chạy tại http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
